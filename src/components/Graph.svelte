3ca02b6c49c158fe97a6ea797f3c4d20ef03b446
Emily Yi <yemily195@gmail.com>
2024-03-14 15:23:25 -0700

    removed duplicate writeup

--- src/components/Graph.svelte
+++ src/components/Graph.svelte
@@ -184,21 +184,3 @@
 </div>
 <div id="tooltip" class="roboto-regular" style="position: absolute; opacity: 0; background-color: rgba(255, 255, 255, 0.5); border: 1px solid #ddd; padding: 10px; border-radius: 4px; pointer-events: none; font-size: 14px;"></div>
 <svg bind:this={svg} width="1200" height="600" class="roboto-regular" style="display: block; margin: auto"></svg>
-<div class="roboto-regular">
-  <h1>Writeup</h1>
-  <h2>What have we done so far?</h2>
-  <p>So far we have set up the project on GitHub and created a basic line plot. We have cleaned the dataset according to the 
-    functionality that we want to implement. As part of cleaning we have dealt with missing values and column labels. We have also 
-    implemented a search box to search for a country and see its temperature change over the years. That was one interactive element 
-    we have implemented. Another interactive element that we have implemented is the tooltip information for the lines on the plot. It 
-    shows the country and the temperature change of the nearest country's line.
-  </p>
-  <h2>What was the most challenging part to design?</h2>
-  <p> The most challenging aspect of the project to design is likely to be  handle missing values, specifically the logic of 
-  back-filling these gaps with the most recent year's temperature change data. This process is  significant for maintaining the 
-  integrity of the dataset and ensuring the smoothness of temperature change over time. Additionally, the integration of a search 
-  box that interacts seamlessly with the visualization also took us a lot of effort to debug to achieve the effect that the system 
-  can respond dynamically to user queries without lags and errors. Besides that,  we are using 'multiselect' library to assist us 
-  on displaying an editable list of countries, complete with a functionality to add or remove entries in our visualization.  
-  </p>
-</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,9 @@
 <script>
   import App from '../components/App.svelte';
   import CO2EmissionsGraph from '../components/CO2App.svelte';
+  import WriteUp from '../components/WriteUp.svelte';
 </script>
 
 <App />
 <CO2EmissionsGraph />
+<WriteUp />
